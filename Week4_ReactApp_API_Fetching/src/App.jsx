ddc55ef2014b885e2b850be171f8fd8b4f3b3227
QTru <23735771.tru@student.iuh.edu.vn>
2026-03-14 01:18:20 +0700

    feat(): ReactApp_API_Fetching Challenge_4

--- Week4_ReactApp_API_Fetching/src/App.jsx
+++ Week4_ReactApp_API_Fetching/src/App.jsx
@@ -3,13 +3,15 @@ import "./App.css";
 import GetAPI from "./components/Challenge_1/GetAPI";
 import LoadingError from "./components/Challenge_2/Loading_Error";
 import GetID from "./components/Challenge_3/GetID";
+import SearchFilter from "./components/Challenge_4/SearchFilter";
 
 function App() {
   return (
     <>
       {/* <GetAPI></GetAPI> */}
       {/* <LoadingError></LoadingError> */}
-      <GetID></GetID>
+      {/* <GetID></GetID> */}
+      <SearchFilter></SearchFilter>
     </>
   );
 }
